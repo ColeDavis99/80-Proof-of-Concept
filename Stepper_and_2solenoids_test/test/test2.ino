5b1ad600d4deed4f490e9ced64febf7016b6a560
ColeDavis99 <coledavis6499@gmail.com>
2024-12-14 15:34:31 -0600

    About to test stepper and solenoid control simultaneously

---
+++ Stepper_and_2solenoids_test/test/test2.ino
@@ -0,0 +1,54 @@
+//This sketch is for bidirection stepper motor control and activating 2 solenoids
+
+// L298N pins - Solenoid controller
+const int solTrgPinA1 = 10; //4 trigger pins
+const int solTrgPinA2 = 11;
+const int solTrgPinB1 = 12;
+const int solTrgPinB2 = 13;
+const int solEnaPinA = 7;   //2 ENA pins (constant 5v)
+const int solEnaPinB = 8;
+
+// TB6600 pins - Stepper motor controller
+const int dirPin = 2;
+const int stepPin = 3;
+
+// Stepper motor constants
+const int stepsPerRev = 6400;
+const int stepDelay = 50;
+
+void setup(){
+    //Set pinmodes
+    pinMode(solTrgPinA1, OUTPUT);
+    pinMode(solTrgPinA2, OUTPUT);
+    pinMode(solTrgPinB1, OUTPUT);
+    pinMode(solTrgPinB2, OUTPUT);
+    pinMode(solEnaPinA, OUTPUT);
+    pinMode(solEnaPinB, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+    pinMode(stepPin, OUTPUT);
+}
+
+
+
+void loop(){
+    //One clockwise revolution
+    digitalWrite(dirPin, HIGH);
+    for(long i=0; i<stepsPerRev; i++){
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(stepDelay);
+    }
+
+    // One counter-clockwise revolution
+    digitalWrite(dirPin, LOW);
+    for (long i = 0; i < stepsPerRev; i++)
+    {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(50);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(50);
+    }
+
+    while(1){}
+}
