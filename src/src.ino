aaf9a2bfdb4bcf72b614d9475ca8b4399226996e
ColeDavis99 <coledavis6499@gmail.com>
2024-12-22 16:50:01 -0600

    Continuing work on src.ino

--- src/src.ino
+++ src/src.ino
@@ -4,6 +4,7 @@
 
 //Test 2: Instanciate a Solenoid object (kinda hijacking it for the LED) using the testPin variable from Header.h
 Solenoid testSolenoid(testPin, whatever);
+int fancyCtr = 0;
 
 void setup(){
   //Test 1: Import the variable testPin directly from "Header.h" (success)
@@ -19,5 +20,10 @@ void loop(){
 //  delay(200);
 
   //Test 2: Use a function of the Solenoid object with pin argument from Header.h
-  testSolenoid.Pour(3000);
+  for(int i=60; i>20; i--){
+    testSolenoid.Pour(i);
+  }
+  for(int i=20; i<60; i++){
+    testSolenoid.Pour(i);
+  }
 }
