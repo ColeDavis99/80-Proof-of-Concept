d869616386d7d55ee6089bcb929337da9504d37e
ColeDavis99 <coledavis6499@gmail.com>
2024-12-25 18:55:19 -0600

    Captured pseudocode for drink pouring algorithm in Controller.h

--- src/Controller.h
+++ src/Controller.h
@@ -23,8 +23,18 @@ public:
     // Moved platform towards stepper motor X revolutions
     void Pull(unsigned long revs);
 
-    // Calculates the most efficient movement path for pouring drink X relative to platform's current position
-    //  TODO
+    /*
+    Calculates the most efficient movement path for pouring drink X relative to platform's current position
+    Algorithm summary: 
+    If(platform is under a solenoid that's part of the recipe)
+        Pour(solenoid)
+    If(There's a tie for the closest left solenoid that's part of the drink and closest right solenoid that's part of the drink)
+        Move to the closest solenoid on the side whose furthest solenoid that's part of the drink is the smaller than the other side, or if that's a tie move to the closest left solenoid that's part of the drink.
+    For(Solenoids_remaining_in_recipe)
+        RunGreedy()
+    */
+    void TODO_1_or_so();
+    void TODO_2_or_so();
 
     // Getters
     // short int getxPos();
